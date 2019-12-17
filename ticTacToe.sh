5a1377adc619e3c7f78f9fd84b088f89fef4dc15
aniketchahare <anichahare@gmail.com>
2019-12-17 15:08:54 +0530

    Added: displayBoard function and cell validation

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-echo " Welcome to Tic Tac Toe"
+echo "Welcome to Tic Tac Toe"
 
 function resetBoard(){
 	for (( i=1; i<=9; i++ ))
@@ -20,5 +20,40 @@ function toss(){
 		echo "Computer play first"
 	fi
 }
-toss
+echo $toss
 
+function displayBoard(){
+	echo "+---+---+---+"
+	echo "| ${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]} |"
+	echo "+---+---+---+"
+	echo "| ${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]} |"
+	echo "+---+---+---+"
+	echo "| ${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]} |" 
+	echo "+---+---+---+"
+}
+
+function validateCell()
+{
+	displayBoard
+	count=0
+	read -p "enter a cell position between 1 to 9 " position
+	for (( i=1;i<=9;i++ ))
+	do
+		if [ $position -eq $i ]
+		then
+			count=1
+			break
+		else
+			count=0
+		fi
+	done
+
+	if [ $count -eq 1 ]
+	then
+	 	gameBoard[$i]=X
+		displayBoard  ${gameBoard[$i]}
+	else
+		validateCell
+	fi
+}
+validateCell
